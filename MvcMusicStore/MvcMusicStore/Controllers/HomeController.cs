55d8c9148e72e73c479fbfa897175c47d55013ef
Callum Cheshire <sftwgot@hotmail.com>
2019-11-14 18:23:36 +0000

    Inital commit

---
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		//
+		// GET: /Home/
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+
+}
